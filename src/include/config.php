28a665dae5b66c45998d7eb3e9b46dadb59a64ba
Leo599Simplon <leo599simplon@gmail.com>
2021-03-19 12:05:00 +0400

    switch connection bdd modif fichier config

--- src/include/config.php
+++ src/include/config.php
@@ -1,9 +1,24 @@
 <?php
 
+if ($_SERVER["REMOTE_ADDR"] == "127.0.0.1") { // Si on est en local on utilise ces identifiants
+    $serveur = "localhost";
+    $dbname = "test";
+    $user = "admin";
+    $password = "test";
+} else { // Sinon on utilise les identifiants de la BDD Heroku
+    $serveur = "us-cdbr-east-03.cleardb.com";
+    $dbname = "b86ba72a9788a4";
+    $user = "13a4dfbb";
+    $password = "heroku_99c077ce9940398";
+}
+
 try { // Connexion à la BDD
-    $pdo = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'admin', 'test', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $pdo = new PDO("mysql:host=$serveur;dbname=$dbname", $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    error_log(date('l jS \of F Y h:i:s A') . ": Connexion à la base de données réussie\r\n", 3, '../var/log.txt');
 } catch (Exception $e) { // Si erreur, on renvoi un message d'erreur
+    error_log(date('l jS \of F Y h:i:s A') . ": Connexion à la base de données impossible\r\n", 3, '../var/log.txt');
     die('Erreur : ' . $e->getMessage());
 }
 
-?>
+
