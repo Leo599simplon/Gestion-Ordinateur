0cdd1c8ab7b482f0b37ef2040af82471bc262a9d
Leo599Simplon <leo599simplon@gmail.com>
2021-03-21 19:42:48 +0400

    front page modification ordinateur et visiteur

--- src/include/updateComp.php
+++ src/include/updateComp.php
@@ -45,23 +45,41 @@ if(isset($_GET['id']) && !empty($_GET['id'])){
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Liste des noms</title>
+    <title>Page de connexion</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <link rel="stylesheet" href="src/style/style.css">
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
+
 <body>
-    <h1>Modifier un Ordinateur</h1>
-    <form method="post">
-        <p>
-            <label for="nom">nom</label>
+
+
+    <div class="container-fluid d-flex align-items-center justify-content-center">
+        <img class="rounded-circle w-25" src="src/ressources/bot_icon.gif" alt="" />
+    </div>
+    </div>
+
+
+    <div class="d-flex flex-column align-items-center container-fluid" id="loginDiv">
+        <h2 class="py-5 mt-5">Modification du nom</h2>
+
+        <form method="post">
+            <div class="form-group mb-3">
+            <label for="nom"></label>
+              
             <input type="text" name="nom" id="nom" value="<?= $result['nom'] ?>">
-        </p>
-        <p>
-            <button>Enregistrer</button>
-        </p>
-        <input type="hidden" name="id" value="<?= $result['id_ordinateur'] ?>">
-    </form>
+                
+            </div>
+           
+            <div class="form-group mb-3 pt-3 d-flex justify-content-center">
+                <button class="btn btn-primary">Enregistrer</button>
+                <input type="hidden" name="id" value="<?= $result['id_ordinateur'] ?>">
+            </div>
+            </div>
+        </form>
+    </div>
 </body>
+
 </html>
 
